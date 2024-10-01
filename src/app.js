0a6699153dccfa34f5f623653daa54d8504c23b8
khushali <amreliyakhushali@gmail.com>
2024-10-01 17:44:14 +0530

    User_changes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./db/dbconnection');
 const router = require('./Routes/v1');
+const authRoutes = require('./Routes/v1/Auth')
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.get('/hello',(req,res)=>{
 // app.use(express.json());
 
 app.use('/v1', router);
+app.use('/v1/login', authRoutes);
 
 const port = 5000
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
